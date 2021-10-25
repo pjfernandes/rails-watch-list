02bdddc230f0432e2b9dbe077ed1be737bb900ce
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-10-24 23:36:22 -0300

    app running

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,42 @@
 class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list)
+  end
+
+  # def create
+  #   @list = List.new(list_params)
+  #   if @list.save
+  #     redirect_to list_path(@list)
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, notice: 'List was successfully destroyed.'
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "lists/new", to: "lists#new" # The `new` route
+  post "lists", to: "lists#create"
+
+  get 'lists', to: 'lists#index'
+  get 'lists/:id', to: 'lists#show', as: :list
+
+  delete "list/:id", to: "lists#destroy", as: :list_delete
+
+  resources :lists, except: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resources :bookmarks, only: [ :new, :create ]
+  end
+
+  resources :bookmarks, only: :destroy
+
 end
